99e4e835e0d6162112ed9d8a5513e159186abf7f
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-05-08 00:36:15 +0600

    updated

--- src/TestAutomation.java
+++ src/TestAutomation.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class TestAutomation {
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class TestAutomation {
+
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://google.com/");
+
+        By langChange = By.xpath("//a[normalize-space()='English']");
+        driver.findElement(langChange).click();
+
+        By searchBtn = By.id("APjFqb");
+        driver.findElement(searchBtn).sendKeys("Dhaka University");
+
+        By searchItem = By.xpath("//span[normalize-space()='Dhaka University']");
+        driver.findElement(searchItem).click();
+
+        By duUrl = By.xpath("//h3[normalize-space()='Dhaka University: Home']");
+        driver.findElement(duUrl).click();
+
+        driver.close();
+
+
+    }
 }
